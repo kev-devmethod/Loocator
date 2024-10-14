b83cad6c47e648f4049403b2a99db112226ac1a6
JalenEvans <jalenemmanuelevans@gmail.com>
2024-10-14 13:33:31 -0400

    Separated main.dart and other pages
    Made a new directory to store other pages created. Made a page designed
    for the map/home screen called map_page.dart. Split the code in
    main.dart amongst map_page.dart to work the same way.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:loocator/pages/map_page.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(32.785811, -79.936280);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Loocator'),
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 16.0,
-          ),
-        ),
+      title: 'Loocator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
+        useMaterial3: true,
       ),
+      home: MapPage(),
     );
   }
+
 }
